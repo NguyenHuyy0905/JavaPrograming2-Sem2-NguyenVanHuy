2820ebf4259462fee68baf3928688402e20a5e2f
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-10-06 02:05:54 +0700

    Update bai tap

---
+++ Java-20231004-Slot02-Lab02/src/Part_01_Getting_Started/Counter.java
@@ -0,0 +1,32 @@
+package Part_01_Getting_Started;
+
+public class Counter {
+    int value;
+    // The forward-count method, that increases the value from 0 to 9,
+    // print out the value in each steps (with 300ms delay)
+    public void count() {
+        value = 0;
+        while (value < 10) {
+            System.out.println("Count: " + (++value));
+            pause(300);
+        }
+    }
+
+
+    // The downward-count method, that dereases the value from 10 to 0,
+    // print out the value in each steps (with 200ms delay)
+    public synchronized void countdown() {
+        value = 10;
+        while (value >= 0) {
+            System.out.println("Countdown: " + (value--));
+            pause(200);
+        }
+    }
+    // Sleep method
+    private synchronized void pause(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {}
+    }
+}
+
---
+++ Java-20231004-Slot02-Lab02/src/Part_01_Getting_Started/SyncDemo.java
@@ -0,0 +1,30 @@
+package Part_01_Getting_Started;
+
+public class SyncDemo {
+    public static void main(String[] args) {
+        final Counter ct = new Counter();
+        // Create new runnable object t1,
+        // therefore t1 is a thread is using Counter object ct
+        Runnable t1 = new Runnable() {
+            public void run() {
+                synchronized(ct) {
+                    ct.count();
+                }
+            }
+        };
+        // Create new runnable object t2,
+        // Therefore t2 is a thread using Counter object ct.
+        // Both t1 and t2 are using ct at the same time,
+        // so ct is called shared-resource
+        Runnable t2 = new Runnable() {
+            public void run() {
+                synchronized(ct) {
+                    ct.countdown();
+                }
+            }
+        };
+        new Thread(t1).start();
+        new Thread(t2).start();
+    }
+}
+
