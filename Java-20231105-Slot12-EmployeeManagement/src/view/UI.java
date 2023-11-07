1aa75b5c62af638344b5d13d63d05dcec3ad6911
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-11-07 15:57:10 +0700

    Update

--- Java-20231105-Slot12-EmployeeManagement/src/view/UI.java
+++ Java-20231105-Slot12-EmployeeManagement/src/view/UI.java
@@ -48,7 +48,75 @@ public class UI {
         int choice = readInt(0, 2);
         return choice;
     }
-    public void start() {};
+    public void start() {
+        while (true) {
+            int choice = mainMenu();
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    boolean flag1 = false;
+                    while (!flag1) {
+                        int choice1 = menu1();
+                        switch (choice1) {
+                            case 0:
+                                flag1 = true;
+                                break;
+                            case 1:
+                                register();
+                                break;
+                            case 2:
+                                login();
+                                break;
+                            default:
+                                throw new AssertionError();
+                        }
+                    }
+                case 2:
+                    boolean flag2 = false;
+                    while (!flag2) {
+                        int choice2 = menu2();
+                        switch (choice2) {
+                            case 0:
+                                flag2 = true;
+                                break;
+                            case 1:
+                                createEmployee();
+                                break;
+                            case 2:
+                                deleteEmployee();
+                                break;
+                            case 3:
+                                modifyInformation();
+                                break;
+                            default:
+                                throw new AssertionError();
+                        }
+                    }
+                case 3:
+                    boolean flag3 = false;
+                    while (!flag3) {
+                        int choice3 = menu3();
+                        switch (choice3) {
+                            case 0:
+                                flag3 = true;
+                                break;
+                            case 1:
+                                findEmployeeByName();
+                                break;
+                            case 2:
+                                findEmployeeById();
+                                break;
+                            default:
+                                throw new AssertionError();
+                        }
+                    }
+                default:
+                    throw new AssertionError();
+            }
+        }
+    };
     private void register() {
         boolean isExistUsername;
         String username;
